20e805f5f13e83a36bf799f116d58759ae8a9ba8
Mike Sperber <sperber@deinprogramm.de>
2022-12-08 14:52:44 +0100

    Update.

--- kotlin/src/main/kotlin/Contract.kt
+++ kotlin/src/main/kotlin/Contract.kt
@@ -1,2 +1,10 @@
-class Contract {
-}
+/*
+ 1. Einfaches Beispiel
+    Zero-Coupon Bond / Zero-Bond:
+    "Ich bekomme Weihnachten 100€."
+ */
+
+data class Date(val iso: String)
+
+sealed interface Contract
+data class ZeroCouponBond(date: Date): Contract
