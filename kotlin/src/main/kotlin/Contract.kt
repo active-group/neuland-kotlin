4e0984669c617f2f2c3c784b165fffc6e88973f6
Mike Sperber <sperber@deinprogramm.de>
2022-12-08 15:05:35 +0100

    Update.

--- kotlin/src/main/kotlin/Contract.kt
+++ kotlin/src/main/kotlin/Contract.kt
@@ -28,6 +28,9 @@ val christmas = Date("2022-12-24")
 val c1 = One(Currency.EUR)
 val c2 = Multiple(100.0, One(Currency.EUR))
 val zcb1 = Later(christmas, Multiple(100.0, One(Currency.EUR)))
+
+fun zeroCouponBond(date: Date, amount: Amount, currency: Currency) =
+    Later(date, Multiple(amount, One(currency)))
 /*
 data class ZeroCouponBond(val date: Date,
                           val amount: Amount,
